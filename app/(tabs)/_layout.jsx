d43ee52f5b7de60ddc1671a19f8ee90312fd0f28
JJaksonn <kottyliko@gmail.com>
2026-02-13 19:06:51 +0200

    API integration

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,56 +1,66 @@
-import { MaterialIcons } from '@expo/vector-icons';
-import { Redirect, Tabs } from 'expo-router';
-import { useAuth } from '../../context/AuthContext';
+import { MaterialIcons } from "@expo/vector-icons";
+import { Redirect, Tabs } from "expo-router";
+import { useAuth } from "../../context/AuthContext";
 
-export default function TabsLayout()
-{
+export default function TabsLayout() {
   const { isLoggedIn } = useAuth();
-  if (!isLoggedIn){ return <Redirect href="/(auth)/login" />}
+  if (!isLoggedIn) {
+    return <Redirect href="/(auth)/login" />;
+  }
   return (
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: '#2a9d8f',
-          tabBarInactiveTintColor: '#555',
-          tabBarStyle: {
-          backgroundColor: '#fff',
-          borderTopColor: '#ddd',
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2a9d8f",
+        tabBarInactiveTintColor: "#555",
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopColor: "#ddd",
           height: 60,
           paddingBottom: 8,
-          },
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+          tabBarShowLabel: false,
         }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="home" color={color} size={size} />
-              
-            ),
-            tabBarShowLabel: false,
-          }}
-        />
-        <Tabs.Screen
-          name="browse"
-          options={{
-            title: 'Browse',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="search" color={color} size={size} />
-            ),
-            tabBarShowLabel: false,
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            title: 'Profile',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="person" color={color} size={size} />
-            ),
-            tabBarShowLabel: false,
-          }}
-        />
-      </Tabs>
+      />
+      <Tabs.Screen
+        name="browse"
+        options={{
+          title: "Browse",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="search" color={color} size={size} />
+          ),
+          tabBarShowLabel: false,
+        }}
+      />
+      <Tabs.Screen
+        name="chatbot"
+        options={{
+          title: "Chatbot",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="smart-toy" size={size} color={color} />
+          ),
+          tabBarShowLabel: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" color={color} size={size} />
+          ),
+          tabBarShowLabel: false,
+        }}
+      />
+    </Tabs>
   );
 }
